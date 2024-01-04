45bad9da00c4e68a6cdc07d7c838f6992c70f00f
Jacob Romero <jromero@captechconsulting.com>
2024-01-04 18:11:25 -0500

    adding comments

--- example-code/status-call.mjs
+++ example-code/status-call.mjs
@@ -2,8 +2,10 @@
 import fetch from 'node-fetch';
 import authDetails from './auth.mjs'
 
+// status endpoint
 let url = 'https://www.thebluealliance.com/api/v3/status';
 
+// making a GET call and addin your auth header
 let options = {
   method: 'GET',
   headers: {
@@ -12,7 +14,24 @@ let options = {
   }
 };
 
+// performs the call
+console.log(`Performing Get Call`)
+console.log(`=======================`)
+console.log(`${url}`)
+
 fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
+  .then(res => {
+    // res -> is the response object.
+    // It holds details about your response
+    
+    // Print status code of response
+    console.log(`response code: ${res.status}`);
+
+    // return body of response (this is in json format so we use res.json())
+    return res.json()
+  }).then(responseBody => {
+    // print out response body in a nice format
+    console.log(`Response body: \n\n ${JSON.stringify(responseBody, null, 2)}`);
+  })
+  // If there is an error print it out.
   .catch(err => console.error('error:' + err));
